c516a48972c2b3799e7ddf1e8e99bd5d4b3e59d2
Robert Sudwarts <robert.sudwarts@gmail.com>
2013-11-19 10:22:41 +0000

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,36 @@
 from setuptools import setup, find_packages
 
+major=0
+minor=1
+micro=0
+
+f = open('README.rst')
+long_description = f.read().strip()
+f.close()
+
+# Requirements to install buffet plugins and engines
+_extra_genshi = ["Genshi >= 0.3.5"]
+_extra_mako = ["Mako >= 0.1.1"]
+
 setup(
-    name='',
-    version='',
-    description='',
-    author='',
-    author_email='',
-    url='',
+    name='tw2.jqplugins.ui_spinner',
+    version='%d.%d.%d' % (major, minor, micro),
+    description='toscawidgets2 widget for TextField with spinner',
+    long_description=long_description,
+    author='Robert Sudwarts',
+    author_email='robert.sudwarts@gmail.com',
+    url='https://github.com/RobertSudwarts/tw2.jqplugins.ui-spinner',
     install_requires=[
         "tw2.core",
-        ## Add other requirements here
-        # "Genshi",
+        "tw2.jquery",
+        "tw2.jqplugins.ui",
         ],
+    extras_require = {
+        'genshi': _extra_genshi,
+        'mako': _extra_mako,
+    },
+    tests_require = ['BeautifulSoup', 'nose', 'FormEncode', 'WebTest',] \
+                     + _extra_genshi + _extra_mako,
     packages=find_packages(exclude=['ez_setup', 'tests']),
     namespace_packages = ['tw2'],
     zip_safe=False,
